b2cec0375c495a0047f29560abd46fa5768ece40
bagawan1 <124817230+bagawan1@users.noreply.github.com>
2024-09-12 06:15:58 +0530

    Update Digital_Imaginary.java

--- src/Digital_Imaginary.java
+++ src/Digital_Imaginary.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
 
-public class DigitalImaginary extends JFrame {
+class DigitalImaginary extends JFrame {
     private JTextField signUpPlayerIdField;
     private JTextField signUpPlayerNameField;
     private JTextField signUpPositionField;
